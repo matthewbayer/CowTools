79eb177427088b80c5b04c3fa4f4c09bd714688f
Matthew Bayer <44325414+matthewbayer@users.noreply.github.com>
2022-07-13 11:09:42 -0400

    Update view_helpers.py

--- app/view_helpers.py
+++ app/view_helpers.py
@@ -1,6 +1,4 @@
 def generate_context(request, extra=None):
     logged_in = request.user.is_authenticated
-    context = {"logged_in": logged_in}
-    if extra:
-        context = {**context, **extra}
+    context = {**context, **extra} if extra else {"logged_in": logged_in}
     return context
